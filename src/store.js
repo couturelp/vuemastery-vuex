74ad749c591c7d0e70d6a8dae165ce6f167c089d
Louis <couturelouisphilippe@gmail.com>
2020-04-21 23:13:15 -0400

    lession3

--- src/store.js
+++ src/store.js
@@ -4,7 +4,15 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Louis-P' },
+    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community']
+  },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length;
+    }
+  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event</h1>
+    <p>This event was created by {{user.name}}</p>
+    <p>There are {{catLength}} categories available</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+  export default {
+    computed:{
+      catLength() {
+        return this.$store.getters.catLength;
+      },
+      ...mapState(['user','categories'])
+    }   
+  }
+</script>
+
+<style scoped>
+
+</style>
